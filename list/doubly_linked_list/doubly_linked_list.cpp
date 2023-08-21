e2067c54c9ac0717e167954cd2616816f83f1cf7
Andrea Maggetto <andreamaggetto40@gmail.com>
2023-08-21 17:40:51 +0200

    implementation

--- list/doubly_linked_list/doubly_linked_list.cpp
+++ list/doubly_linked_list/doubly_linked_list.cpp
@@ -5,12 +5,42 @@ doubly_linked_list<T>::doubly_linked_list() = default;
 
 template<typename T>
 doubly_linked_list<T>::doubly_linked_list(const T& init, size_t init_size) : size(0){
-    for(size_t i = 0; i < init_size; ++i) push_back(init);
+    for(size; size < init_size; ++size){
+        std::unique_ptr<node> to_add = std::make_unique<node>();
+        to_add->info = init;
+        to_add->prev = tail;
+        to_add->next = nullptr;
+
+        if(!head){
+            head = std::move(to_add);
+            tail = head.get();
+        }
+        else{
+            tail->next = std::move(to_add);
+            tail = tail->next.get();
+        }
+    }
 };
 
+template<typename T>
+doubly_linked_list<T>::doubly_linked_list(doubly_linked_list<T>&& dll){
+    head = std::move(dll.head);
+    tail = dll.tail;
+    size = dll.size;    
 
+    dll.tail = nullptr;
+    dll.size = 0;
+};
 
+template<typename T>
+doubly_linked_list<T>::doubly_linked_list(const doubly_linked_list<T>& dll){            
+    node* it = dll.head.get();
 
+    while(it != nullptr){
+        push_back(it->info);
+        it = it->next.get();
+    }
+};
 
 template<typename T>
 doubly_linked_list<T>& push_back(const T& el){       
